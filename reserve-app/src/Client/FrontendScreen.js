1d8a50db8df5acb25cbc96c81049386fb9162c30
Mohamed Riyaz <sathickshakila785@gmail.com>
2024-02-27 21:11:34 +0530

    new add

--- reserve-app/src/Client/FrontendScreen.js
+++ reserve-app/src/Client/FrontendScreen.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import '../index.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function FrontendScreen() {
+  useEffect(()=>{
+    AOS.init({duration:2000})
+  },[])
+
   return (
     <>
     <div className='landing'>
         <div className='text-center'>
-            <h2 id="h21" className='typewriter'>
+            <h2 id="h21" className='typewriter' data-aos="zoom-in">
             Movie<i style={{borderRadius:"17px",border:"1px solid red",width:"123px",height:"13px",backgroundColor:"white",color:"red"}}>E</i>card
               </h2>
          <h5 id="h22">"it All Starts Here</h5>
